1e2bb7fd01d4e922cd1d782dbe71c48d9f134e31
Pushpank <pushpankq@gmail.com>
2019-02-13 12:57:30 +0530

    Create generic function for linear searching

--- Contents.swift
+++ Contents.swift
@@ -1,47 +1,18 @@
 import UIKit
 
-var str = "Hello, playground"
-
-// create a constant integer array 
-let array = [10, 20, 80, 30, 60, 50,
-    110, 100, 130, 170]
-
-// searching element
-let searchItem = 100
-
-// Basic Programming Function
-func linearSearching(array:[Int], searchElement:Int) -> Int {
-    
-    if array.isEmpty {
+// linear generic search function 
+func linearSearch<T:Equatable>(arr:[T], searchItem : T) -> Int? {
+    for (index,item) in arr.enumerated() where item == searchItem {
         
-        // if array is blank 
-        return -1
-        
-    } else {
-        
-        for (index, value) in array.enumerated() {
-           
-            if value  == searchElement {
-                
-                return index
-            }
-        }
+        return index
     }
-    
-    return -1
+    return nil
 }
 
-let index = linearSearching(array: array, searchElement: searchItem)
 
-if index == -1 {
-    // If element is not found in the array 
 
-    print("\(searchItem) is not found")
-
-} else {
-
-    print("\(searchItem) is at index \(index)")
-
-}
+let arra = [10,20,80,30,60,50]
+let searchItem = 20
+let newIndex = linearSearch(arr: arra, searchItem: searchItem)
 
 
